3e0c11188ad1f7aefb32812a826635c7632281b3
Arturo <arturvargas9226@gmail.com>
2019-12-02 21:52:11 -0600

    [update] mongo schemas

--- 8_Mongo/server/app.js
+++ 8_Mongo/server/app.js
@@ -9,11 +9,31 @@ mongoose.connect(process.env.MongoUrl, {useNewUrlParser: true}, (err) => {
 });
 
 const app = express();
+const Project = require('./models/projects');
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('<h1>Server</h1>');
 })
+
+app.post('/new/project', (req, res) => {
+    let params = req.body;
+    console.log(params);
+    
+    let newProject = new Project({
+        name: params.name,
+        language: params.language,
+        version: params.version,
+        url: params.url
+    });
+    newProject.save((err, project) => {
+        if(err){
+            res.status(400).json({err});
+        }
+        res.json({project});
+    })
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto: ${process.env.PORT}`);
---
+++ 8_Mongo/server/models/projects.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: String,
+        required: false
+    },
+    version:{
+        type: Number,
+        required: false
+    },
+    url: {
+        type: String,
+        required: true,
+        default: ''
+    }
+},{timestamps: true})
+
+
+const Project = mongoose.model('project', projectSchema);
+
+module.exports = Project;
